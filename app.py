496d20e13e4e0c3f14532a5fe97998741f933472
Kumamnotsume <stephen_magee_@hotmail.co.uk>
2024-07-26 00:56:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 @socketio.on('join')
 def on_join(data):
     room = data['room']
